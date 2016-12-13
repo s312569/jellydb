3a4991be0d807dc11e315dbd46e7d38a58091cdb
Jason Mulvenna <jason.mulvenna@gmail.com>
2016-12-13 22:03:39 +1000

    Change

--- src/clj/jellydb/models/proteomics.clj
+++ src/clj/jellydb/models/proteomics.clj
@@ -5,7 +5,8 @@
             [clj-fasta.core :as fa]
             [me.raynes.fs :as fs]
             [clj-tandem.core :as xt]
-            [clj-commons-exec :as exec]))
+            [clj-commons-exec :as exec]
+            [clojure.string :as st]))
 
 (def test-pds {:dir "/home/jason/Dropbox/jellydb/resources/test-data/chironex-20130410"
                :alkylation :iaa
@@ -20,10 +21,6 @@
 (def alkylation-parameter
   {:iaa "57.021464@C"})
 
-(defn process-params
-  [m]
-  )
-
 ;; utilities
 
 (defn- new-ext
@@ -31,21 +28,21 @@
   (str (fs/file (fs/parent file) (str (fs/base-name file oe) ne))))
 
 (defn run-external
-  [comm]
-  (try
-    (let [o @(exec/sh comm)]
-      (if-not (= 0 (:exit o))
-        (println
-         (throw
-          (Exception.
-           (str "Error: Problem executing " (first comm) " - " "Error out: " (:out o)))))
-        (:out o)))))
+  ([comm] (run-external comm nil))
+  ([comm log-file]
+   (let [o @(exec/sh comm)]
+     (if-not (= 0 (:exit o))
+       (do (if log-file (spit log-file (:out o) :append true))
+           (throw (Exception. (str "Error: Problem executing " (first comm)
+                                   " - " "Error out: " (:out o)))))
+       (do (if log-file (spit log-file (:out o) :append true))
+           (:out o))))))
 
 ;; pipeline
 
 (defn- make-reversed
   [s]
-  (vector s {:accession (str (:accession s) "_rev")
+  (vector s {:accession (str "rev_" (:accession s))
              :description (str "rev_" (:description s))
              :sequence (->> (apply str (reverse (:sequence s))))}))
 
@@ -72,26 +69,67 @@
                (map str)))))
 
 (defn- tandem2pepxml
-  [{:keys [tfiles] :as m}]
+  [{:keys [tfiles log-file] :as m}]
   (assoc m :pfiles
          (doall
           (map #(let [nf (new-ext % "xtML" "pepxml")]
-                  (run-external ["Tandem2XML" % nf])
+                  (run-external ["Tandem2XML" % nf] log-file)
                   nf)
                tfiles))))
 
 (defn- xinteract
-  [{:keys [pfiles] :as m}]
-  (let [odir (fs/parent (first pfiles))
-        out (str (fs/file odir "interact.tandem.pep.xml"))]
-    (assoc m :xinteract (run-external
-                         (concat ["xinteract" "-OARPd" "-drev_" (str "-N"out)]
-                                 (doall (map str (fs/glob (str odir "/" "*.pepxml")))))))))
+  [{:keys [dir log-file] :as m}]
+  (let [out (str (fs/file dir "interact.tandem.pep.xml"))]
+    (assoc m :xinteract-file
+           (do (run-external
+                (concat ["xinteract" "-OARPd" "-drev_" (str "-N"out)]
+                        (doall (map str (fs/glob (str dir "/" "*.pepxml")))))
+                log-file)
+               out))))
+
+(defn- interprophet
+  [{:keys [xinteract-file dir log-file] :as m}]
+  (let [out (str (fs/file dir "iprophet.pep.xml"))]
+    (assoc m :interprophet-file
+           (do (run-external ["InterProphetParser" "DECOY=rev_" "THREADS=4" xinteract-file out]
+                             log-file)
+               out))))
+
+(defn- protein-prophet
+  [{:keys [interprophet-file dir log-file] :as m}]
+  (let [out (str (fs/file dir "iprophet.prot.xml"))]
+    (assoc m :prot-proph-file
+           (do (run-external ["ProteinProphet" interprophet-file out "IPROPHET"]
+                             log-file)
+               out))))
+
+(defn- mayu
+  [{:keys [dir log-file db interprophet-file] :as m}]
+  (let [out (str (fs/file dir "mayu"))]
+    (-> (assoc m :mayu-file
+               (do (run-external ["Mayu.pl" "-A" interprophet-file
+                                  "-C" db "-E" "rev_" "-G" "0.01"
+                                  "-H" "51" "-I" "2"
+                                  "-P" "protFDR=0.01:t" "-M" out "-v"]
+                                 log-file)
+                   (str out "_psm_protFDR0.01_t_1.07.csv")))
+        (assoc :mayu-score
+               (with-open [r (io/reader (str out "_psm_protFDR0.01_t_1.07.csv"))]
+                 (->> (line-seq r)
+                      rest
+                      (map #(st/split % #","))
+                      (map #(nth % 4))
+                      (map #(Float/parseFloat %))
+                      (apply min)))))))
 
 (defn process-directory
   [{:keys [dir alkylation enzyme var-mods con-mods dataset] :as m}]
-  (-> (generate-db m)
-      get-sfiles
-      tandem-search
-      tandem2pepxml
-      xinteract))
+  (let [ml (assoc m :log-file (ut/working-file "search-log"))]
+    (-> (generate-db ml)
+        get-sfiles
+        tandem-search
+        tandem2pepxml
+        xinteract
+        interprophet
+        protein-prophet
+        mayu)))
